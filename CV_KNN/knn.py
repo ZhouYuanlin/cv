225bb29106616f1a4e00d28334f356d34902e971
周园林 <3270630993@qq.com>
2017-09-19 16:22:40 +0800

    knn

--- CV_KNN/knn.py
+++ CV_KNN/knn.py
@@ -8,3 +8,11 @@ def load_CIFAR_batch(filename):
 
     with open(filename,'r') as f:
         dataDict = pickle.load(f)
+        X = dataDict['data']
+        Y = dataDict['labels']
+        X = X.reshape(10000,3,32,32).transpose(0,2,3,1).astype("float")
+        Y = np.array(Y)
+        return  X,Y
+def load_CIFAR10(ROOT):
+    xs=[]
+    ys=[]
